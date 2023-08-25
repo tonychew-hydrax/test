628a666884bad9aeb4bf3cb08865ee7096513bb5
tonychew-hydrax <133841619+tonychew-hydrax@users.noreply.github.com>
2023-08-25 16:17:42 +0800

    Update derivation.js

---
+++ derivation.js
@@ -0,0 +1,12 @@
+import { ethers } from "./dist/ethers.min.js";
+
+const hdwallet =  () => {
+  let hdnode = ethers.utils.HDNode.fromExtendedKey(
+    "xpub6EU9wCZtFWLvSuqz6Apd4xWjx2LWToJhAaswLRvb9XEKcRS9p4f63bGNhVboDqxDfERqGtA3gkxFj6CYynxXKmsrkV8HEjcVcGu4orqxmzQ",
+    "m/44'/60'/0'/0"
+  );
+  let wallet = hdnode.derivePath("0");
+  console.log(wallet.address);
+};
+
+hdwallet();
